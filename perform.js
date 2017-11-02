412e4232b765eda880dfb37ff7beabf4078fab97
zhangfan <413273029@qq.com>
2017-11-02 17:08:13 +0800

    transform

---
+++ perform.js
@@ -0,0 +1,17 @@
+cheet('p i k a c h u', function () {
+  window.setTimeout(openMouth, 200);
+  playPikachu();
+});
+
+function playPikachu() {
+  var pikachu = document.getElementById('audio');
+  audio.play();
+}
+
+function openMouth() {
+  var mouth = document.getElementById('open');
+  mouth.classList.add('active');
+  window.setTimeout(function () {
+    mouth.classList.remove('active');
+  }, 1500);
+}
